e08e270c1ff754cebe93e1c3c0092ad04dfef4bc
torhankarina <torhankarina@gmail.com>
2025-10-20 22:26:37 -0500

    Update raining_test.cpp

--- tests/raining_test.cpp
+++ tests/raining_test.cpp
@@ -5,6 +5,6 @@
 
 #include "../src/raining.hpp"
 
-TEST_CASE( "it returns Hello World" ) {
-    REQUIRE( hello() == "Hello World!" );
-}
+//TEST_CASE( "it returns Hello World" ) {
+ //   REQUIRE( hello() == "Hello World!" );
+//}
